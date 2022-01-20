b027aa46ed849f87684237d33ba0125a89ee050c
Joshua Brian Johansen <joshuabrian.johansen@dk.experis.com>
2022-01-20 16:00:25 +0100

    Minor changes to index.html, new features in app.js
    index.html:
        - changed image size
        - changed title
    
    app.js:
        - Added handleBuy functionality
        - Fixed bug where loanAmount could go negative when banking pay

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const laptopTitleElement = document.getElementById("laptop-title");
 const laptopDescriptionElement = document.getElementById("laptop-description");
 const laptopPriceElement = document.getElementById("laptop-price");
 const laptopImageElement = document.getElementById("laptop-image");
+const buyNowButtonElement = document.getElementById("buy-now-button");
 
 let computers = [];
 let pay = 0;
@@ -17,7 +18,8 @@ let balance = 0;
 let loanAmount = 0;
 let haveLoan = false;
 let repayLoanButton;
-let outstandingLoan = 0;
+let selectedLaptop = computers[0];
+let person = {};
 
 
 fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
@@ -28,6 +30,7 @@ fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
     //Calls the addComputerToMenu function for every element in the computers array
 const addComputersToMenu = (computers) => {
     computers.forEach(computer => addComputerToMenu(computer));
+    
     computerFeaturesListElement.innerText = computers[0].specs;
     
     laptopTitleElement.innerText = computers[0].title;
@@ -39,6 +42,7 @@ const addComputersToMenu = (computers) => {
     const laptopImageAttribute = document.createAttribute("src");
     laptopImageAttribute.value = `https://noroff-komputer-store-api.herokuapp.com/${computers[0].image}`;
     laptopImageElement.setAttributeNode(laptopImageAttribute);
+    return person.currentlyLookingAtPrice = `${computers[0].price}`
 }
 
 //Creates and option in the laptop dropdown and adds the current laptop from the computers array to that option
@@ -50,7 +54,8 @@ const addComputerToMenu = (computer) => {
 }
 
 const handleLaptopMenuChange = event => {
-    const selectedLaptop = computers[event.target.selectedIndex];
+    selectedLaptop = computers[event.target.selectedIndex];
+    
     computerFeaturesListElement.innerText = selectedLaptop.specs;
 
     const laptopImageAttribute = document.createAttribute("src");
@@ -60,32 +65,41 @@ const handleLaptopMenuChange = event => {
     laptopTitleElement.innerText = selectedLaptop.title;
     laptopDescriptionElement.innerText = selectedLaptop.description;
     laptopPriceElement.innerText = `${selectedLaptop.price} NOK`;
+    return person.currentlyLookingAtPrice = `${selectedLaptop.price}`;
 }
 
 //Handles the work button functionality
 const handleWork = () => {
     payElement.innerText = `Pay: ${pay + 100}`;
-    pay = pay + 100;
+    pay += 100;
 }
 
 //Handles the Bank button functionality
 const handleBankPay = () => {
-    if(haveLoan === true) {
-        balanceElement.innerText = `Balance: ${parseInt(balance) + (parseInt(pay) * 0.9)}`
-        balance = balance + (pay * 0.9);
-        outstandingLoan -= pay * 0.10;
-        payElement.innerText = `Pay: ${parseInt(pay) - parseInt(pay)}`;
+    let percentageOffPay = pay * 0.1
+    if(haveLoan === true && percentageOffPay <= loanAmount) {
+        balance += pay * 0.9;
+        loanAmount -= percentageOffPay;
+        pay -= pay;
+        balanceElement.innerText = `Balance: ${parseInt(balance)}`
+        payElement.innerText = `Pay: ${parseInt(pay)}`;
+    } else if(haveLoan === true && percentageOffPay > loanAmount) {
+        loanAmount -= percentageOffPay;
+        loanAmount = Math.abs(loanAmount);
+        balance += pay * 0.9 + loanAmount;
+        loanAmount = 0;
+        pay -= pay;
+        balanceElement.innerText = `Balance: ${parseInt(balance)}`
+        payElement.innerText = `Pay: ${parseInt(pay)}`;
+        } else {
+        balance += pay;
         pay = pay - pay;
+        balanceElement.innerText = `Balance: ${parseInt(balance)}`;
+        payElement.innerText = `Pay: ${parseInt(pay)}`;
     }
-    else {
-        balanceElement.innerText = `Balance: ${parseInt(balance) + parseInt(pay)}`;
-        balance = balance + pay;
-        payElement.innerText = `Pay: ${parseInt(pay) - parseInt(pay)}`;
-        pay = pay - pay;
-    }
-    if(outstandingLoan === 0) {
+    if(loanAmount === 0) {
         repayLoanButtonElement.removeChild(repayLoanButton);
-        haveLoan === false;
+        haveLoan = false;
     }
 }
 
@@ -93,9 +107,8 @@ const handleBankPay = () => {
 const handleLoan = () => {
     loanAmount = prompt("Please enter the amount you would like to loan",100);
     if(loanAmount <= balance * 2 && haveLoan === false) {
-        balanceElement.innerText = `Balance: ${parseInt(loanAmount) + parseInt(balance)}`;
         balance = parseInt(loanAmount) + balance;
-        outstandingLoan = loanAmount;
+        balanceElement.innerText = `Balance: ${parseInt(balance)}`;
         
         haveLoan = true;
 
@@ -109,9 +122,9 @@ const handleLoan = () => {
 
 const handleLoanRepayment = () => {
     if(balance >= loanAmount) {
-        balanceElement.innerText = `Balance: ${parseInt(balance) - parseInt(loanAmount)}`;
-        balance = balance - parseInt(loanAmount);
-        loanAmount = parseInt(loanAmount) - parseInt(loanAmount);
+        balance -= parseInt(loanAmount);
+        loanAmount -= parseInt(loanAmount);
+        balanceElement.innerText = `Balance: ${parseInt(balance)}`;
         repayLoanButtonElement.removeChild(repayLoanButton);
 
         haveLoan = false;
@@ -120,18 +133,30 @@ const handleLoanRepayment = () => {
     }
 }
 
-const handleFeatureListSplit = (computers) => {
-    let computerFeaturesArray = computers.specs.split(",");
-    for (let index = 0; index < computerFeaturesArray.length; index++) {
-        let computerFeature = document.createElement("li")
-        computerFeature.innerText = `${computerFeaturesArray[index]}`;
-        computerFeaturesListElement.appendChild(computerFeature);
+const handleBuyNow = () => {
+    let laptopPrice = parseInt(person.currentlyLookingAtPrice);
+    console.log(laptopPrice)
+    if(balance >= parseInt(laptopPrice)) {
+        balance -= parseInt(laptopPrice);
+        balanceElement.innerText = `Balance: ${parseInt(balance)}`
+        alert("Thank you for your purchase");
+    } else {
+        alert("You do not have enough money to buy that!")
     }
 }
 
+// const handleFeatureListSplit = (computers) => {
+//     let computerFeaturesArray = computers.specs.split(",");
+//     for (let index = 0; index < computerFeaturesArray.length; index++) {
+//         let computerFeature = document.createElement("li")
+//         computerFeature.innerText = `${computerFeaturesArray[index]}`;
+//         computerFeaturesListElement.appendChild(computerFeature);
+//     }
+// }
 
 laptopsElement.addEventListener("change", handleLaptopMenuChange);
 repayLoanButtonElement.addEventListener("click", handleLoanRepayment);
 bankMoneyButtonElement.addEventListener("click", handleBankPay);
 doWorkButtonElement.addEventListener("click", handleWork);
 loanButtonElement.addEventListener("click", handleLoan);
+buyNowButtonElement.addEventListener("click", handleBuyNow);
